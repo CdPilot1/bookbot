826a4d74710f7f06ae2dde7781906c7a30be16f2
CdPilot1 <lachlan5890@gmail.com>
2025-07-09 18:50:44 +0930

    formatted, main now returns sorted letter count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import count_characters
+from stats import sorted_char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -8,6 +9,12 @@ def get_book_text(path_to_file):
 from stats import count_characters
 
 def main():
-    print(get_num_words('books/frankenstein.txt'), "words found in the document.")
-    print(count_characters('books/frankenstein.txt'))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print("Found", get_num_words('books/frankenstein.txt'), "total words")
+    print("--------- Character Count -------")
+    sorted_chars = sorted_char_count('books/frankenstein.txt')
+    for char_dict in sorted_chars:
+        print(f"{char_dict['char']}: {char_dict['count']}")
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
+
 def get_num_words(path_to_file):
     word_count = get_book_text('books/frankenstein.txt').split()
     return len(word_count)
@@ -13,3 +14,11 @@ def count_characters(path_to_file):
         else:
             char_counts[char] = 1
     return char_counts
+
+def sorted_char_count(path_to_file):
+    def sort_on(dict_item):
+        return dict_item['count']
+    char_count_dict = count_characters(path_to_file)
+    list_of_dicts = [{'char': char, 'count': count} for char, count in char_count_dict.items() if char.isalpha()] 
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
