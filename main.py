eeb1e2ebd7ea0ea8c59fc6b41cbcd559ee684595
CdPilot1 <lachlan5890@gmail.com>
2025-07-09 16:19:50 +0930

    Get text added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as file:
+        return file.read()
+
+def main():
+    print(get_book_text('books/frankenstein.txt'))
+
+main()
