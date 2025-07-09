9464a96c7fec5ab9e489f3776fd18724ed8eaaa5
CdPilot1 <lachlan5890@gmail.com>
2025-07-09 16:33:56 +0930

    main.py now returns word count instead of whole document

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
 
+def get_word_count(path_to_file):
+    word_count = get_book_text('books/frankenstein.txt').split()
+    return len(word_count)
+
 def main():
-    print(get_book_text('books/frankenstein.txt'))
+    print(get_word_count('books/frankenstein.txt'), "words found in the document.")
 
 main()
